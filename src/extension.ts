b9b4a2ff52c10058239c23295e800c80cceff413
rodhiam <straight.tea.lipton@gmail.com>
2020-03-25 11:01:44 +0900

    impl postMessage

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,31 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+let wpanel: vscode.WebviewPanel;
+let editor: vscode.TextEditor;
+
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "vscode-mindyaml" is now active!');
-	let panel: vscode.WebviewPanel;
-	let editor: vscode.TextEditor;
+
+	vscode.window.onDidChangeActiveTextEditor(
+		editor => {
+			if (editor === undefined) {
+				return;
+			}
+		
+			if (editor) {
+				editor = editor;
+			}
+			wpanel.webview.postMessage({
+				command: "text",
+				text: editor.document.getText()
+			});
+			vscode.window.showInformationMessage("post message to " + wpanel.title);
+		},
+		null,
+		context.subscriptions
+	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand("mindyaml.show", () => {
@@ -21,6 +41,13 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			);
 			panel.webview.html = getWebviewContent();
+
+			panel.webview.onDidReceiveMessage(
+				message => {
+					vscode.window.showInformationMessage('receive message');
+				},
+				null);
+			wpanel = panel;
 		})
 	);
 }
@@ -33,8 +60,20 @@ export function getWebviewContent() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-        <div id="app"></div>
-        <p>hello world</p>
+		<p id="text">hello world</p>
+		<script>
+		const text = document.getElementById('text');
+
+		// Handle the message inside the webview
+		window.addEventListener('message', event => {
+			const message = event.data; // The JSON data our extension sent
+			switch (message.command) {
+				case 'text':
+					text.textContent = message.text;
+					break;
+			}
+		});
+		</script>
 	</body>
 	</html>`;
 }
