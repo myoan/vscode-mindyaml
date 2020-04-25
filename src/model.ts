56dc4e355a69536312a50576a29165498bdae05e
myoan <motoki.yoan@gmail.com>
2020-04-26 03:33:20 +0900

    add Node class

---
+++ src/model.ts
@@ -0,0 +1,46 @@
+import * as yaml from 'js-yaml';
+
+class Node {
+    name: string;
+    children: Node[];
+
+    constructor(name: string) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(node: Node) {
+        this.children.push(node);
+    }
+
+    toHash() {
+        return {name: this.name};
+    }
+}
+
+export class Model {
+    raw: object;
+    constructor(data: object) {
+        this.raw = data;
+    }
+
+    convert() {
+        const sample = yaml.safeLoad('- hoge');
+        var result: Node[] = [];
+        if (Array.isArray(this.raw)) {
+            this.raw.forEach(e => {
+                result.push(new Node(e));
+            });
+            return result.map(n => n.toHash());
+        }
+        return result.map(n => n.toHash());
+    }
+
+    export() {
+        if (this.raw === {}) {
+            console.log('default');
+            return {};
+        }
+        return this.convert();
+    }
+}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,22 @@
 import * as assert from 'assert';
+import * as yaml from 'js-yaml';
+import {Model} from '../../model';
 
-
-suite('Extension Test Suite', () => {
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('YAML data suite', () => {
+	test('Model test', () => {
+		const testCase = [
+			{
+				input: {},
+				expect: []
+			},
+			{
+				input: ["hoge"],
+				expect: [{"name": "hoge"}]
+			}
+		];
+		testCase.forEach(tc => {
+			const model = new Model(tc['input']);
+			assert.equal(JSON.stringify(tc['expect']), JSON.stringify(model.export()));
+		});
 	});
 });
